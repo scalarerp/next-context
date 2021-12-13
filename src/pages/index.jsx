78f88ace078af3a6e217b8f6e3d8cd8442e7eab8
MauricioGedrat <mauriciogedrat@gmail.com>
2021-12-13 17:23:29 -0300

    remoção importação da foonte

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,7 @@
-import Head from 'next/head'
-
 import React from 'react'
 
 function Index() {
-    return (
-        <>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-                    rel="stylesheet"
-                />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-            </Head>
-        </>
-    )
+    return <></>
 }
 
 export default Index
