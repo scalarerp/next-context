3c6a7af0c9478dddb3b6101623f5c1a6b8e52214
Jonas Gedrat <jonas.gedrat@fullgauge.com.br>
2021-12-13 17:00:19 -0300

    criação objeto navLinks

---
+++ src/objetcs.ts
@@ -0,0 +1,18 @@
+export const navLinks = {
+    home: {
+        name: 'Inicio',
+        link: '/',
+        className: '',
+    },
+    about: {
+        name: 'Sobre',
+        link: '/about',
+        className: '',
+    },
+
+    contact: {
+        name: 'Contato',
+        link: '/contact',
+        className: '',
+    },
+}
